f3d7cb2e45fcfaedd583636158f8949e537ce290
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-10-09 00:46:07 +0300

    0.9 Add Feature#2

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from datetime import datetime
 from user_list_generator import CustomUserListGenerator
 from data_fetcher import CustomDataFetcher
 from translator import CustomTranslator
+from online_count import UserStatsController
+from UserStatsUserController import UserStatsHistoryController
 
-if __name__ == "__main__":
+def main():
     custom_base_url = "https://sef.podkolzin.consulting/api/users/lastSeen"
+
     while True:
         try:
             desired_custom_count = int(input("Enter the number of users you want to use: "))
             max_offset = int(input("Enter max number of offset you want to use: "))
+
             if desired_custom_count > 0:
                 break
             else:
@@ -29,5 +33,34 @@ if __name__ == "__main__":
     custom_translator = CustomTranslator()
     custom_user_list_generator.custom_when_online(custom_users, current_time)
 
-    for custom_user in custom_users:
-        print(f"{custom_user.username} {custom_translator.translate(custom_user.last_seen, custom_lang)}")
+    history_controller = UserStatsHistoryController(custom_data)  # Передаем custom_data
+
+    while True:
+        print("Enter '1' to check users online stats, '2' to get user history, '3' to list all users, 'q' to quit: ")
+        command = input()
+
+        if command == '1':
+            date = input("Enter the date (YYYY-MM-DD): ")
+            controller = UserStatsController()
+            result = controller.get_users_online_stats(date)
+            print(f"Users Online on {date}: {result.UsersOnline}")
+        elif command == '2':
+            user_id = input("Enter the user ID: ")
+            date = input("Enter the date (YYYY-MM-DD-HH:MM): ")
+            history_result, status_code = history_controller.get_user_history(user_id, date)
+            if status_code == 404:
+                print("User not found.")
+            else:
+                print(f"wasUserOnline: {history_result['wasUserOnline']}, nearestOnlineTime: {history_result['nearestOnlineTime']}")
+        elif command == '3':
+            print("List of all users:")
+            for user in custom_users:
+                translated_last_seen = custom_translator.translate(user.last_seen, custom_lang)
+                print(f"Username: {user.username}, Last Seen: {translated_last_seen}, Is Online: {user.is_online}")
+        elif command == 'q':
+            break
+        else:
+            print("Invalid command. Please enter '1', '2', '3', or 'q'.")
+
+if __name__ == "__main__":
+    main()

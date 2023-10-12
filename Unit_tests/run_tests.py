4c3d0ba83410e39b9070b2b943020bc031276bdc
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-10-12 12:03:13 +0300

    Add integration test

--- Unit_tests/run_tests.py
+++ Unit_tests/run_tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 test_loader = unittest.TestLoader()
-test_suite = test_loader.discover(start_dir='.', pattern='test_*.py')
+test_suite = test_loader.discover(start_dir='Unit_tests/', pattern='test_*.py')
 
 if __name__ == '__main__':
     unittest.TextTestRunner().run(test_suite)
